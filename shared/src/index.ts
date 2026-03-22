92b06288cbd09d628b81d2ce492ce53eec484879
Valentin (Val) Mekhonoshina <70847744+geminicodes@users.noreply.github.com>
2026-03-22 15:45:27 +0300

    Delete shared directory

--- shared/src/index.ts
+++
@@ -1,5 +0,0 @@
-export type { JobPosting, DetectedLanguage, ToneLabel, ToneAnalysis } from "./types/job";
-export type { AnalysisResult, RepoScore } from "./types/analysis";
-export type { GitHubRepo } from "./types/github";
-export type { GenerateReadmeRequest, GenerateReadmeResponse } from "./types/readme";
-
